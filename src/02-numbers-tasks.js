245feed78b38118990f0ca831aae46156e06db83
Mikhail <mishania261282@mail.ru>
2023-10-30 01:30:35 +0300

    resolve

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -221,7 +221,7 @@ function isPrime(n) {
  *   toNumber(new Number(42), 0) => 42
  */
 function toNumber(value, def) {
-  return typeof Number(value) === 'number' ? value : def;
+  return typeof (Number(value)) === 'number' ? Number(value) : def;
 }
 
 module.exports = {
